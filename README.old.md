1f5bd8d45c576f4f58476ada78cf746fd963aca7
DOANLE <doanlecong1811@gmail.com>
2021-09-08 23:54:39 +0700

    Change Readme

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# Some-Useful-React-Hooks
-Some useful react hooks which are essential for every react project 
---
+++ src/usefulHooks/index.js
@@ -0,0 +1,23 @@
+import useToggle from "./1-useToggle/useToggle";
+import useTimeout from "./2-useTimeout/useTimeout";
+import useDebounce from  "./3-useDebounce/useDebounce";
+import useUpdateEffect from "./4-useUpdateEffect/useUpdateEffect";
+import useArray from  "./5-useArray/useArray";
+import usePrevious from  "./6-usePrevious/usePrevious";
+import useStateWithHistory from "./7-useStateWithHistory/useStateWithHistory";
+import useStorage from "./8-useStorage/useStorage";
+import useAsync from "./9-useAsync/useAsync";
+import useFetch from "./10-useFetch/useFetch";
+
+export {
+    useToggle,
+    useTimeout,
+    useDebounce,
+    useUpdateEffect,
+    useArray,
+    usePrevious,
+    useStateWithHistory,
+    useStorage,
+    useAsync,
+    useFetch,
+}
